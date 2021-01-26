d630fedf69853dc253305f035b41fbf7455d57de
Şafak Soylu <dawnoble@hotmail.com>
2021-01-26 14:38:23 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,5 +7,9 @@ fi
 
 systemctl stop ngrok.service
 systemctl disable ngrok.service
+
+
 rm /lib/systemd/system/ngrok.service
+rm /lib/systemd/system/socks5.service
 rm -rf /opt/ngrok
+rm -rf /opt/socks5
